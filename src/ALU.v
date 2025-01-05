d0e4770e4f3c160c8fa0d09b0e9465ab93e302d8
Suhaib Sawalha <68230953+SuhaibSawalha@users.noreply.github.com>
2025-01-05 23:33:13 +0200

    Update ALU.v

--- src/ALU.v
+++ src/ALU.v
@@ -1,17 +1,17 @@
 module ALU (
     input [15:0] Rs, Rt,        // Inputs Rs and Rt
-    input [3:0] OPcode,         // 4-bit Opcode
+    input [2:0] OPcode,         // 4-bit Opcode
     output reg [15:0] Rd        // 16-bit Output
 );
 
     always @(*) begin
         case (OPcode)
-            4'b0000: Rd = Rs & Rt;          // AND
-            4'b0001: Rd = Rs + Rt;          // ADD
-            4'b0010: Rd = Rs - Rt;          // SUB
-            4'b0011: Rd = Rs << Rt;         // SLL
-            4'b0100: Rd = Rs >> Rt;         // SRL
-            default: Rd = 16'b0;            // Default case for undefined OPcodes
+            3'b000: Rd = Rs & Rt;          // AND
+            3'b001: Rd = Rs + Rt;          // ADD
+            3'b010: Rd = Rs - Rt;          // SUB
+            3'b011: Rd = Rs << Rt;         // SLL
+            3'b100: Rd = Rs >> Rt;         // SRL
+            default: Rd = 16'b0;           // Default case for undefined OPcodes
         endcase
     end
 
