f954b0f0ee301d4127bdeaaa4ee3cbd55ef83c10
Suhaib Sawalha <68230953+SuhaibSawalha@users.noreply.github.com>
2025-01-05 23:41:14 +0200

    Update ALU_Control.v

--- src/ALU_Control.v
+++ src/ALU_Control.v
@@ -1,14 +1,19 @@
-module ALU_Control(
-  input	[3:0] opcode,
-  output reg	[2:0] ALUop
-);
-
-  always @(*)
-    case(opcode)
-		`AND: ALUop = 3'b000; 
-    `ADD: ALUop = 3'b001;
-		`SUB: ALUop = 3'b010;
-    `SLL: ALUop = 3'b011;
-    `SRL: ALUop = 3'b100;
-    endcase
+module(
+	input      [3:0] Op,
+	input      [2:0] func,	
+	output reg [2:0] ALUop);		  
+	
+	always @(*)
+		case(Op)
+			4'b0000: ALUop = func;
+			`ANDI:   ALUop = 3'b000;
+			`ADDI:   ALUop = 3'b001;
+			`LW:  	 ALUop = 3'b001;
+			`SW:     ALUop = 3'b001;	 
+			`BEQ:    ALUop = 3'b010;
+			`BNE:    ALUop = 3'b010;
+			`FOR:    ALUop = 3'b001;
+			default: ALUop = 3'bxxx;
+		endcase	 
+		
 endmodule
