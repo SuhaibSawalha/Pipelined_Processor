40aa616357876151d46bd474e8855176bef24b9c
Abdalhameed Maree <145791169+AbdalhameedMaree@users.noreply.github.com>
2025-01-13 22:29:42 +0200

    Update True_Buf.v

--- AbdComments/True_Buf.v
+++ AbdComments/True_Buf.v
@@ -1,8 +1,14 @@
-module True_Buf(input [15:0] in, input clk, input call, output reg [15:0] out);
+module True_Buf(input [15:0] in,   // 16-bit input signal
+                input clk,           // Clock signal
+                input call,          // Control signal to trigger the output assignment
+                output reg [15:0] out // 16-bit output signal
+);
 
-    always @(negedge clk) begin			 
+    // Always block triggered by the negative edge of the clock (negedge)
+    always @(negedge clk) begin
+        // If the 'call' signal is active, assign the input 'in' to the output 'out'
         if (call)
-            out = in;
+            out = in;  // Transfer input to output when 'call' is high
     end
 
 endmodule
