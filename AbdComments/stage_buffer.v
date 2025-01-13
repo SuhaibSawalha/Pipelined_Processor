40613b31c39e862a05ac7a7edbf8d79f90d45acd
Abdalhameed Maree <145791169+AbdalhameedMaree@users.noreply.github.com>
2025-01-13 22:30:43 +0200

    Update stage_buffer.v

--- AbdComments/stage_buffer.v
+++ AbdComments/stage_buffer.v
@@ -1,17 +1,25 @@
 module stage_buffer (CLK, I, O);
 	
+	// Parameter n defines the width of the input and output data (default is 16 bits)
 	parameter n = 16;
-	input CLK;		 
-	input [n-1:0] I;
-	output reg [n-1:0] O;
-	reg [n-1:0] val;  
 	
+	// Input and output declarations
+	input CLK;            // Clock input
+	input [n-1:0] I;      // Input data (n-bit wide)
+	output reg [n-1:0] O; // Output data (n-bit wide)
+	
+	// Internal register to hold the value between clock cycles
+	reg [n-1:0] val;  
+
+	// Initial block to set the initial value of 'val' on startup
 	initial
 		val <= I;
 	
+	// On the negative edge of the clock, update 'val' with the value of 'I'
 	always @(negedge CLK)
 		val <= I;
 		
+	// On the positive edge of the clock, update 'O' with the value of 'val'
 	always @(posedge CLK)
 		O <= val;
 		
