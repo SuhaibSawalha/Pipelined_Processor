94ffbf1f4b7397624a6c254ef3ca48e20d049fe8
Abdalhameed Maree <145791169+AbdalhameedMaree@users.noreply.github.com>
2025-01-13 21:55:03 +0200

    Update Add.v

--- AbdComments/Add.v
+++ AbdComments/Add.v
@@ -1,9 +1,10 @@
 module Add (
-    input [15:0] a,
-    input [15:0] b,
-    output [15:0] sum
+    input [15:0] a,          // 16-bit input operand 'a'
+    input [15:0] b,          // 16-bit input operand 'b'
+    output [15:0] sum        // 16-bit output for the result of addition
 );
 
+    // Perform addition of the two 16-bit inputs 'a' and 'b'
     assign sum = a + b;
 
 endmodule
