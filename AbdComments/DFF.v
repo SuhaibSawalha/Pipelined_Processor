c20959940c437a292d7a8ca3cfa802fb0d8ec989
Abdalhameed Maree <145791169+AbdalhameedMaree@users.noreply.github.com>
2025-01-13 21:58:21 +0200

    Update DFF.v

--- AbdComments/DFF.v
+++ AbdComments/DFF.v
@@ -1,9 +1,13 @@
-module DFF (PC, input CLK, input first);
-	
-	output reg [15:0] PC;
-	
-	always @(posedge CLK)
-		if (first) 
-			PC <= 0;
-			
+module DFF (
+    output reg [15:0] PC,   // 16-bit output register to hold the value of PC (Program Counter)
+    input CLK,              // Clock signal for triggering the flip-flop
+    input first             // Control signal to initialize PC to zero on the first clock cycle
+);
+
+    // Always block triggered on the rising edge of the clock signal (posedge CLK)
+    always @(posedge CLK)
+        if (first) 
+            // If 'first' is asserted, reset PC to 0
+            PC <= 0;
+
 endmodule
