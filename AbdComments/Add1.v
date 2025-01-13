6293128e97557efc137c67261d208941341e18a4
Abdalhameed Maree <145791169+AbdalhameedMaree@users.noreply.github.com>
2025-01-13 21:55:38 +0200

    Update Add1.v

--- AbdComments/Add1.v
+++ AbdComments/Add1.v
@@ -1,8 +1,9 @@
 module Add1 (
-    input [15:0] a,
-    output [15:0] sum
+    input [15:0] a,          // 16-bit input operand 'a'
+    output [15:0] sum        // 16-bit output for the result of incrementing 'a' by 1
 );
 
+    // Perform addition: Increment the 16-bit input 'a' by 1
     assign sum = a + 1;
 
 endmodule
